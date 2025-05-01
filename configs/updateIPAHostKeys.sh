b0061538b5002302d66c084b3d0a393be2c368da
Will Bedu <23764568+wbedu@users.noreply.github.com>
2025-05-01 08:45:49 -0500

    better clean up

--- configs/updateIPAHostKeys.sh
+++ configs/updateIPAHostKeys.sh
@@ -10,9 +10,14 @@ if [ -z "$host" ]; then
   exit 1
 fi
 
+
+hostkeyPath=$(mktemp -d)
+pushd $hostkeyPath
 ssh-keyscan -p $port -t rsa,ed25519 $host \
   | awk '{$1=$1; print $2, $3}' \
   > cleaned_keys.pub > hostkeys.pub;
 while read key; do
   ipa host-mod $host --sshpubkey="$key"
 done < hostkeys.pub
+popd
+rm -rf $hostkeyPath
