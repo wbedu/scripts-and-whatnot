659c2f0d7378250110fea705faf51496c4444bbe
Will Bedu <23764568+wbedu@users.noreply.github.com>
2025-05-01 08:43:50 -0500

    add ipa host keys script

---
+++ configs/updateIPAHostKeys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+host=$1
+port="${port:=22}"
+
+if [ -z "$host" ]; then
+  echo "Error: Host is not defined."
+  echo "Usage: $0 <host> [port]"
+  exit 1
+fi
+
+ssh-keyscan -p $port -t rsa,ed25519 $host \
+  | awk '{$1=$1; print $2, $3}' \
+  > cleaned_keys.pub > hostkeys.pub;
+while read key; do
+  ipa host-mod $host --sshpubkey="$key"
+done < hostkeys.pub
